c77cd2963d4116595463b2bdb8c9c49038144146
Gabriel Cardoso <gabrieloc6@gmail.com>
2026-01-29 12:03:33 -0300

    tratando token expirado e respostas das exceções

--- src/main/java/com/javanauta/usuario/controller/GlobalExceptionHandler.java
+++ src/main/java/com/javanauta/usuario/controller/GlobalExceptionHandler.java
@@ -4,22 +4,35 @@ import com.javanauta.usuario.infrastructure.exceptions.ConflictException;
 import com.javanauta.usuario.infrastructure.exceptions.IllegalArgumentException;
 import com.javanauta.usuario.infrastructure.exceptions.ResourceNotFoundException;
 import com.javanauta.usuario.infrastructure.exceptions.UnauthorizedException;
+import com.javanauta.usuario.infrastructure.exceptions.dto.ErrorResponseDTO;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.time.LocalDateTime;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex){
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorResponseDTO> handleResourceNotFoundException(ResourceNotFoundException ex,
+                                                                            HttpServletRequest request){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(buildError(
+                HttpStatus.NOT_FOUND.value(),
+                ex.getMessage(),
+                request.getRequestURI()
+                ));
     }
 
     @ExceptionHandler(ConflictException.class)
-    public ResponseEntity<String> handleConflictException(ConflictException ex){
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    public ResponseEntity<ErrorResponseDTO> handleConflictException(ConflictException ex, HttpServletRequest request){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(buildError(
+                HttpStatus.CONFLICT.value(),
+                ex.getMessage(),
+                request.getRequestURI()
+        ));
     }
 
     @ExceptionHandler(UnauthorizedException.class)
@@ -30,5 +43,14 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgumentExcption(IllegalArgumentException ex){
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    private ErrorResponseDTO buildError(int status, String message, String path){
+        return ErrorResponseDTO.builder()
+                .timestamp(LocalDateTime.now())
+                .message(message)
+                .status(status)
+                .path(path)
+                .build();
     }
 }
